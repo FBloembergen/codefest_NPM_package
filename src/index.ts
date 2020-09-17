0679ad7cca0e0113fcb3bf088fc4ebde20d2c8c4
Ferry Bloembergen <fbloembergen@gmail.com>
2020-09-17 22:34:05 +0200

    woork wooork

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 /**
  * Yes, it will be awesome.
  */
-export const awesomeFunction = async () => {
+export const awesomeFunction = () => {
     logISODate(`Processing request...`);
     try {
         fs.copyFileSync('./README.md', './README_2.md');
@@ -13,16 +13,10 @@ export const awesomeFunction = async () => {
         }
     };
     logISODate(`Made copy of README.md`);
-    await new Promise((resolve, reject) => {
-        setTimeout(() => resolve(), 5000);
-    });
     logISODate(`### DELETING ALL FILES IN CURRENT DIRECTORY ###`);
-    await new Promise((resolve, reject) => {
-        try {
-            fs.unlinkSync('./README_2.md')
-        } catch (e) { }
-        setTimeout(() => resolve(), 5000);
-    });
+    try {
+        fs.unlinkSync('./README_2.md')
+    } catch (e) { }
     logISODate(`...`);
     logISODate(`Just kidding 💩`);
 };
